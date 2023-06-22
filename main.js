31bb9f9ed5367237eda1b72cf32d566d0a85aa3b
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-06-21 23:09:35 -0500

    cambio_CV1_portfolio

--- main.js
+++ main.js
@@ -14,8 +14,3 @@ function seleccionar() {
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-
-document.getElementById('btnDescargar').addEventListener('click', function() {
-    let url = 'public/CV-CANVA-BaironSandoval.pdf';
-    window.open(url, '_blank');
-  });
