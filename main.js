8b2632ec544cdf744c401f2cfba32ce613574676
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-11-14 12:48:52 -0500

    changes2

--- main.js
+++ main.js
@@ -10,17 +10,17 @@ for (var i = 0; i < selectores.length; i++) {
   selectores[i].addEventListener("click", mostrarOcultarMenu);
 }
 
-const swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  loop: true,
-  coverflowEffect: {
-    depth: 500,
-    modifier: 1,
-    slideShadows: false,
-    rotate: 0,
-    stretch: 0,
-  }
-})
+// const swiper = new Swiper(".mySwiper", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   loop: true,
+//   coverflowEffect: {
+//     depth: 500,
+//     modifier: 1,
+//     slideShadows: false,
+//     rotate: 0,
+//     stretch: 0,
+//   }
+// })
