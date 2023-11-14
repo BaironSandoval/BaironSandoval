29f9a3405724bbf213bd96f3837e25a66766a88f
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-11-14 11:01:49 -0500

    changes styles

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ let selectores = document.querySelectorAll("#seleccionar");
 for (var i = 0; i < selectores.length; i++) {
   selectores[i].addEventListener("click", mostrarOcultarMenu);
 }
+
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+    depth: 500,
+    modifier: 1,
+    slideShadows: false,
+    rotate: 0,
+    stretch: 0,
+  }
+})
